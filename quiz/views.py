7fbd1278cba5bf82aae49b4264acc89850c216ed
amandasj <amanda.sjosward@gmail.com>
2015-08-06 10:59:44 +0200

    Changed the definition of question in views

--- quiz/views.py
+++ quiz/views.py
@@ -31,8 +31,17 @@ def quiz(request, slug):
 	}
 	return render(request, "quiz/startup.html", context)
 
-def question(request):
-	return render(request, "quiz/fragor.html")
+def question(request, slug, number):
+	context = {
+		“question_number”: number,
+	    	"question": u"Hur många bultar har ölandsbron?",
+		"answer1": u"12",
+	   	"answer2": u"66 400",
+	    	"answer3": u"7 428 954",
+	    	"quiz_slug": slug,
+	}
+	return render(request, "quiz/fragor.html", context)
+
 
 def completed(request):
 	return render(request, "quiz/resultat.html")
