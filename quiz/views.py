ec0c5249b1e7b578a8ddd6b67754c4ba0dd7e3e1
amandasj <amanda.sjosward@gmail.com>
2015-08-06 09:58:34 +0200

    But HTML def (request and return) in views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def startpage(request):
+	return render(request, “quiz/index.html”)
+
+def quiz(request):
+	return render(request, “quiz/startup.html”)
+
+def question(request):
+	return render(request, “quiz/fragor.html”)
+
+def completed(request):
+	return render(request, “quiz/resultat.html”)
+
+
+
