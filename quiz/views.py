f92b6d959fe66c6f10528d3f28b2c6cc88d96007
amandasj <amanda.sjosward@gmail.com>
2015-08-06 11:07:38 +0200

    Ändrade URL:en, lade in ny kod i resultat.html

--- quiz/views.py
+++ quiz/views.py
@@ -43,8 +43,13 @@ def question(request, slug, number):
 	return render(request, "quiz/fragor.html", context)
 
 
-def completed(request):
-	return render(request, "quiz/resultat.html")
+def completed(request, slug):
+	context = {
+	    	"correct": 12,
+	    	"total": 20,
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/resultat.html", context)
 
 
 
