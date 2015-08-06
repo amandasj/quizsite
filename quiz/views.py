7038370acdcd64e42c9f1f1322ca85cd81dbe61f
amandasj <amanda.sjosward@gmail.com>
2015-08-06 10:15:07 +0200

    Changed quotemarks in views.py

--- quiz/views.py
+++ quiz/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def startpage(request):
-	return render(request, “quiz/index.html”)
+	return render(request, "quiz/index.html")
 
 def quiz(request):
-	return render(request, “quiz/startup.html”)
+	return render(request, "quiz/startup.html")
 
 def question(request):
-	return render(request, “quiz/fragor.html”)
+	return render(request, "quiz/fragor.html")
 
 def completed(request):
-	return render(request, “quiz/resultat.html”)
+	return render(request, "quiz/resultat.html")
 
 
 
