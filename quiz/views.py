28b01788516dd8fb6d42ef6518f79b685b61a4c2
amandasj <amanda.sjosward@gmail.com>
2015-08-06 10:39:05 +0200

    Lade till innehåll i views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,28 @@
+# coding: utf-8
+
 from django.shortcuts import render
 
 # Create your views here.
 
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
 def startpage(request):
-	return render(request, "quiz/index.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/index.html", context)
 
 def quiz(request):
 	return render(request, "quiz/startup.html")
@@ -14,19 +33,6 @@ def question(request):
 def completed(request):
 	return render(request, "quiz/resultat.html")
 
-quizzes = {
-	"klassiker": {
-   		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
-	},
-	"fotboll": {
-	   	"name": "Största fotbollslagen",
-	   	"description": "Kan du dina lag?"
-	},
-	"kanda-hackare": {
-	    	"name": "Världens mest kända hackare",
-	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
-}
 
 
 
